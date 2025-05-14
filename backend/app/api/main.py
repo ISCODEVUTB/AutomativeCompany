f0d713a05170bec607efa4a583242fe034484176
Ssebatiaan <sebapaternina@hotmail.com>
2025-05-14 03:16:35 -0500

    Añadido microservicio de inventario con operaciones CRUD

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,14 +1,29 @@
 from fastapi import APIRouter
+from fastapi import FastAPI
 
-from app.api.routes import items, login, private, users, utils
+# Importa las rutas
+from backend.app.api.routes import items, login, private, users, utils
 from app.core.config import settings
 
+# Crea la instancia de FastAPI
+app = FastAPI()
+
+# Crea el router principal para las rutas
 api_router = APIRouter()
+
+# Incluye las rutas específicas en el router
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(items.router)
 
-
+# Solo incluye el router privado si está en el entorno local
 if settings.ENVIRONMENT == "local":
     api_router.include_router(private.router)
+
+# Finalmente, incluye el router en la app
+app.include_router(api_router)
+
+
+
+
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -27,7 +27,7 @@ def parse_cors(v: Any) -> list[str] | str:
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         # Use top level .env file (one level above ./backend/)
-        env_file="../.env",
+        env_file=".env",
         env_ignore_empty=True,
         extra="ignore",
     )
@@ -56,6 +56,7 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
+
 
     @computed_field  # type: ignore[prop-decorator]
     @property
